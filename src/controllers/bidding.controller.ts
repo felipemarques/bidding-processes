596e7336f2290fba9465c7337b78f86a1303c11d
Felipe Marques <contato@felipemarques.com.br>
2023-09-02 20:34:54 -0300

    feat: add new route to get biddings

--- src/controllers/bidding.controller.ts
+++ src/controllers/bidding.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post } from '@nestjs/common'
+import { Controller, Get, Post, Query } from '@nestjs/common'
+import { ListProcessQueryDto } from 'src/dto/list-process-query.dto'
 import { BatchOperationService } from 'src/services/batchOperation.service'
 
 @Controller('bidding')
@@ -8,5 +9,10 @@ export class BiddingController {
   @Post('start-extraction')
   startBatch(): void {
     this.batchService.startExtraction()
+  }
+
+  @Get()
+  async getProcess(@Query() query: ListProcessQueryDto) {
+    return this.batchService.getProcess(query)
   }
 }
